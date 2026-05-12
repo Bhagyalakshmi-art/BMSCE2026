ac8a7b2b50380e89aa8b922757068ba8de9b80de
Bhagyalakshmi-art <bhkolaki.81@gmail.com>
2026-05-12 16:40:20 +0530

    Modified

--- selection_sort.py
+++ selection_sort.py
@@ -1,11 +1,12 @@
-numbers=list(map(int,input("Enter the numbers:").split()))
-for u in range(len(numbers)):
-    min_index=u
-    for j in range(u+1,len(numbers)):
-        if numbers[j]<numbers[min_index]:
-            min_index=j
-        numbers[u],numbers[min_index]=numbers[min_index],numbers[u]
-            
-print(numbers)
+def sel_sort(numbers):
+    numbers=list(map(int,input("Enter the numbers:").split()))
+    for u in range(len(numbers)):
+        min_index=u
+        for j in range(u+1,len(numbers)):
+            if numbers[j]<numbers[min_index]:
+                min_index=j
+            numbers[u],numbers[min_index]=numbers[min_index],numbers[u]
+    return numbers
+
     
         
